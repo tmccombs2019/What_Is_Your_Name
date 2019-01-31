b6c51f7af11c29e11bebad7e3df4faf85d1427b3
tmccombs2019 <tracey.mccombs@verizon.net>
2019-01-31 18:09:31 -0500

    v1

--- What_Is_Your_Name/Program.cs
+++ What_Is_Your_Name/Program.cs
@@ -5,8 +5,17 @@ namespace What_Is_Your_Name
     class Program
     {
         static void Main(string[] args)
+
+            //ask user for input
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+                   
+                string name = "Tracey MCombs";
+
+                Console.WriteLine(name);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey(true);
+               
+                     
+        }//end void
+    }//end class
+}//end namespace
